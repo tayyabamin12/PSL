bffeb7f12dafa8541851390d0552a99302a41008
tayyabamin12 <tayyab.riu@gmail.com>
2017-03-31 18:16:11 +0500

    Initial commit

---
+++ app/src/test/java/com/moss/psl2017scheduleandteams/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.moss.psl2017scheduleandteams;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
